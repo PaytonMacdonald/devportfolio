50c2c66e0d19612d98a824bddb85942e769f8c71
Payton Macdonald <paytonmacdonald49@gmail.com>
2021-06-17 18:34:31 -0600

    github first push

---
+++ devportfolio.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'payton49.us.auth0.com'
+export const audience = 'https://bugtracker'
+export const clientId = '8ybvUP8iOak3pUUHfE7Pg4VImC9pt4Yg'
